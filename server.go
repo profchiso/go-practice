16a1210d8a386477807a14f65bb8bad4659275a3
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-01-27 11:23:02 +0100

    andle get all users

--- server.go
+++ server.go
@@ -1,26 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-type user struct {
+type User struct {
 	name      string
 	age       int
 	gender    string
 	isMarried bool
 }
 
-var users []user
+var Users []User
 
 func main() {
-	users = []user{
+	Users = []User{
 		{name: "chinedu", age: 24, gender: "male", isMarried: true},
 		{name: "chinedu", age: 24, gender: "male", isMarried: true},
 	}
-	fmt.Println(users)
+	fmt.Println(Users)
 	handleRequest()
 }
 
@@ -29,7 +30,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home endpoint hit")
 
 }
+func getAllUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get all users endpoint hit")
+	json.NewEncoder(w).Encode(Users)
+}
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/users", getAllUsers)
 	log.Fatal(http.ListenAndServe(":5001", nil))
 }
