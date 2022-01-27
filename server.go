c4b143accf2c3b602cd3742e501d1a85457f70fb
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-01-27 11:04:33 +0100

    fix some issues

--- server.go
+++ server.go
@@ -6,16 +6,30 @@ import (
 	"net/http"
 )
 
+type user struct {
+	name      string
+	age       int
+	gender    string
+	isMarried bool
+}
+
+var users []user
+
 func main() {
+	users = []user{
+		{name: "chinedu", age: 24, gender: "male", isMarried: true},
+		{name: "chinedu", age: 24, gender: "male", isMarried: true},
+	}
+	fmt.Println(users)
 	handleRequest()
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to go programing api")
-	fmt.Println("home endpont hit")
+	fmt.Println("home endpoint hit")
 
 }
 func handleRequest() {
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":6000", nil))
+	log.Fatal(http.ListenAndServe(":5001", nil))
 }
